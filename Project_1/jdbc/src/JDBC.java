7dee735a0764c90ebf8e97673b72811a8db58977
Wesley <wlum@uci.edu>
2016-04-03 01:50:00 -0700

    Detailed sqlexception msg. Cleaned up code layout

--- Project_1/jdbc/src/JDBC.java
+++ Project_1/jdbc/src/JDBC.java
@@ -3,13 +3,15 @@ package jdbc;
 import java.sql.*;
 import java.util.*;
 
+
 public class JDBC
 {
 	
+
 	private static boolean isNumber(String string){
 		
 		for (int i = 0; i < string.length(); i++){
-			
+
 			if (!Character.isDigit(string.charAt(i)))
 				return false; 
 			
@@ -18,6 +20,7 @@ public class JDBC
 		return true; 
 		
 	}
+
 	
 	public static void print(Connection connection, Scanner scan) throws Exception{
 		
@@ -31,8 +34,10 @@ public class JDBC
 		
 		if (isNumber(firstQuery))
 			result = select.executeQuery("SELECT m.id, m.title, m.year, m.director, IF (m.banner_url IS NULL, \'NULL\', m.banner_url) AS BannerURLExists, IF (m.trailer_url IS NULL, \'NULL\', m.trailer_url) AS TrailerURLExists FROM stars_in_movies sim, movies m, stars s WHERE s.id = sim.star_id AND m.id = sim.movie_id AND (s.id = " + firstQuery + ")");
+		
 		else if (secondQuery.equals(""))
 			result = select.executeQuery("SELECT DISTINCT m.id, m.title, m.year, m.director, IF (m.banner_url IS NULL, \'NULL\', m.banner_url) AS BannerURLExists, IF (m.trailer_url IS NULL, \'NULL\', m.trailer_url) AS TrailerURLExists FROM stars_in_movies sim, movies m, stars s WHERE s.id = sim.star_id AND m.id = sim.movie_id AND (s.first_name = \'" + firstQuery + "\' OR s.last_name = \'" + firstQuery + "\')");
+		
 		else
 			result = select.executeQuery("SELECT DISTINCT m.id, m.title, m.year, m.director, IF (m.banner_url IS NULL, \'NULL\', m.banner_url) AS BannerURLExists, IF (m.trailer_url IS NULL, \'NULL\', m.trailer_url) AS TrailerURLExists FROM stars_in_movies sim, movies m, stars s WHERE s.id = sim.star_id AND m.id = sim.movie_id AND ((s.first_name = \'"  + firstQuery + "\' AND s.last_name = \'" + secondQuery + "\') OR (s.first_name =  \'" + firstQuery + "\' OR s.last_name = \'" + secondQuery + "\'))");
 		
@@ -52,6 +57,7 @@ public class JDBC
 		
 	}
 
+
 	public static void insert_star(Connection connection, Scanner scan) throws Exception {
 		
 		System.out.println("Please Enter the Star ID: ");
@@ -67,8 +73,9 @@ public class JDBC
 		
 		String updateString; 
 		
-		if (firstName.equals(""))
+		if(firstName.equals(""))
 			updateString = "INSERT INTO stars VALUES(" + starID + ", \'" + "" + "\', \'" + lastName + "\', \'" + date + "\', \'" + photoURL + "\')"; 
+		
 		else
 			updateString = "INSERT INTO stars VALUES(" + starID + ", \'" + firstName + "\', \'" + lastName + "\', \'" + date + "\', \'" + photoURL + "\')";
 		
@@ -77,6 +84,7 @@ public class JDBC
 		updateStatement.close(); 
 		
 	}
+
 
 	public static void insert_customer(Connection connection, Scanner scan) throws Exception {
 		
@@ -103,6 +111,7 @@ public class JDBC
 
 	}
 
+
 	public static void delete_customer(Connection connection, Scanner scan) throws Exception {
 		
 		System.out.println("Please enter the Customer ID: ");
@@ -116,17 +125,21 @@ public class JDBC
 
 	}
 
+
 	public static void print_metadata(Connection connection) throws SQLException {
 		
 		ResultSet rs = connection.getMetaData().getTables(null, null, "%", null); 
-		while (rs.next()){
+		
+		while(rs.next()){
 			
 			System.out.println("Table: " + rs.getString(3));
 	        Statement select = connection.createStatement();
 	        ResultSet result = select.executeQuery("Select * from " + rs.getString(3));
 	        ResultSetMetaData metadata = result.getMetaData();
+	        
 	        for (int i = 1; i <= metadata.getColumnCount(); i++)
 	        	System.out.println("Type of column "+ metadata.getColumnName(i) + ": " + metadata.getColumnTypeName(i));
+
 	        System.out.println();
 	        
 	        select.close();
@@ -136,34 +149,41 @@ public class JDBC
 		
 	}
 
+
 	public static void sql_command(Connection connection, String query) throws SQLException {
+
 		String split[] = query.split(" ");
 
 		if(split[0].equalsIgnoreCase("select")){
+
 			Statement statement = connection.createStatement();
 			ResultSet result; 
 			result = statement.executeQuery(query);
 			int columns = result.getMetaData().getColumnCount();
 			String attributes = "";
 
-			for (int i = 1; i <= columns; i ++){
+			for(int i = 1; i <= columns; i ++)
 				attributes += (result.getMetaData().getColumnName(i)) + " ";
-			}
+
 			System.out.println("\nAttributes: " + attributes);
 
 			while(result.next()) {
+
 				String line = "";
-				for (int j = 1; j <= columns; j++){
+				
+				for (int j = 1; j <= columns; j++)
 					line = line + result.getString(j) + " ";
-				}
+
 				System.out.println();
 				System.out.println(line);
+
 			}
 			
 			result.close(); 
 			statement.close(); 
 			
 		}
+
 		else if(split[0].equalsIgnoreCase("update")){
 			
 			PreparedStatement updateStatement = connection.prepareStatement(query);
@@ -172,6 +192,7 @@ public class JDBC
 			updateStatement.close(); 
 			
 		}
+
 		else if(split[0].equalsIgnoreCase("insert")){
 			
 			PreparedStatement updateStatement = connection.prepareStatement(query);
@@ -180,6 +201,7 @@ public class JDBC
 			updateStatement.close(); 
 			
 		}
+
 		else if(split[0].equalsIgnoreCase("delete")){
 			
 			PreparedStatement updateStatement = connection.prepareStatement(query);
@@ -188,34 +210,47 @@ public class JDBC
 			updateStatement.close(); 
 			
 		}
-		else{
-			
+
+		else
 			System.out.println("Error in MYSQL Syntax");
-			
-		}		
+	
 	}
+
 
 	public static void exit_menu(Connection connection) throws Exception {
+
 		connection.close();
-		System.out.println("\nSuccessfully disconnected from your database");						
+		System.out.println("\nSuccessfully disconnected from your database");	
+
 	}
 
+
 	public static boolean exit_menu_and_program(Connection connection) throws Exception {
+
 		exit_menu(connection);
 		System.out.println("Successfully exit the program");							
 		return true;
+
 	}
 
+
 	public static boolean exit_program(Scanner scan) {
+
 		String decision;
 		do{
+
 			System.out.println("Do you want to exit the program? [Y/N]");
 			decision = scan.nextLine();
-		}while((!decision.equalsIgnoreCase("Y")) && (!decision.equalsIgnoreCase("N")));
+
+		} while((!decision.equalsIgnoreCase("Y")) && (!decision.equalsIgnoreCase("N")));
+
 		if(decision.equalsIgnoreCase("Y"))
 			System.out.println("\nSuccessfully exit the program");
+
 		return decision.equalsIgnoreCase("Y");
+
 	}
+
 	
 	public static void main(String[] arg) throws Exception {
 
@@ -251,50 +286,45 @@ public class JDBC
 						System.out.println("\nEnter a command [Print, Insert Star, Insert Customer, Delete Customer, Print Metadata, SQL command, Exit Menu, Exit Program]:");
 						command = scan.nextLine();
 	
-						if(command.equalsIgnoreCase("Print")){
-				
+						if(command.equalsIgnoreCase("Print"))
 							print(connection, scan);
-							
-						}
-						else if(command.equalsIgnoreCase("Insert Star")){
-							
+
+						else if(command.equalsIgnoreCase("Insert Star"))
 							insert_star(connection, scan);
 							
-						}
-						else if(command.equalsIgnoreCase("Insert Customer")){
-	
+						else if(command.equalsIgnoreCase("Insert Customer"))
 							insert_customer(connection, scan); 
 	
-						}
-						else if(command.equalsIgnoreCase("Delete Customer")){
-							
+						else if(command.equalsIgnoreCase("Delete Customer"))
 							delete_customer(connection, scan); 
-	
-						}
-						else if(command.equalsIgnoreCase("Print Metadata")){
-							
+
+						else if(command.equalsIgnoreCase("Print Metadata"))
 							print_metadata(connection);
 							
-						}
 						else if(command.equalsIgnoreCase("SQL Command")){
+
 							System.out.println("\nType a valid SQL command:");
 							sql = scan.nextLine();
 							sql_command(connection, sql);
+
 						}
+
 						else if(command.equalsIgnoreCase("Exit Menu")){
+
 							exit_menu(connection);
 							break;
+
 						}
+
 						else if(command.equalsIgnoreCase("Exit Program")){
+
 							exit_program = exit_menu_and_program(connection);
 							break;
+
 						}
 						
-						else {
-							
+						else 
 							System.out.println("Invalid Command. Please try again."); 
-							
-						}
 						
 					}
 					
@@ -302,24 +332,42 @@ public class JDBC
 						
 						error = e.getClass().getCanonicalName();
 						
-						if(error.equals("com.mysql.jdbc.exceptions.MySQLSyntaxErrorException")){
+						if(error.equals("com.mysql.jdbc.exceptions.MySQLSyntaxErrorException"))
 							System.out.println("\nError in MYSQL Syntax");
-						}
-						else if (error.equals("com.mysql.jdbc.exceptions.MySQLIntegrityConstraintViolationException")){
+
+						else if (error.equals("com.mysql.jdbc.exceptions.MySQLIntegrityConstraintViolationException"))
 							System.out.println("\nError! Cannot perform command to MYSQL DataBase.");
-						}
 						
 					}
+
 				}
+
 			}
+
 			catch(SQLException e){
+
 				error = e.getClass().getCanonicalName();
-				if(error.equals("java.sql.SQLException")){
+
+				if(e.getErrorCode() == 1045)
+					System.out.println("\nIncorrect credentials");
+
+				else if(e.getErrorCode() == 1049)
+					System.out.println("\nDatabase does not exist");
+
+				else if(error.equals("java.sql.SQLException")){
+
 					System.out.println();
 					System.out.println(e.getMessage());
+
 				}
+
 				exit_program = exit_program(scan);
+
 			}
+
 		}
+
 	}
+
+
 }
